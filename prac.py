d8ce8b6f295b0da2f1ef427373dbfff3fb842665
hardik <hardik.gondha1994@gmail.com>
2019-06-30 03:04:27 +0530

    new changes

---
+++ prac.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 27 23:13:56 2019
+
+@author: Hardik
+"""
+for num in [20, 11, 9, 66, 4, 89, 44]:
+    if num%2 == 0:
+        #pass
+    else:
+        print(num)
