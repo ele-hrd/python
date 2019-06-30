72cd3724821df7b6c477fdda2dc34780f42c9d88
ele-hrd <52368878+ele-hrd@users.noreply.github.com>
2019-06-30 12:18:56 +0530

    new change

--- string_manipulation/prac.py
+++ string_manipulation/prac.py
@@ -4,8 +4,8 @@ Created on Thu Jun 27 23:13:56 2019
 
 @author: Hardik
 """
-for num in [20, 11, 9, 66, 4, 89, 44]:
-    if num%2 == 0:
+for number in [20, 11, 9, 66, 4, 89, 44]:
+    if number %2 == 0:
         #pass
     else:
-        print(num)
+        print(number)
